d9234da4712756f3b840a3817f3d60ca914e6e48
nunezdelgadomarcos <nunezdelgadomarcos@gmail.com>
2024-11-25 12:15:26 +0000

    First Commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import HomePageContent from "../components/HomePage/HomePageContent";
+import HomePageHeader from "../components/HomePage/HomePageHeader";
+
+const HomePage = () => {
+    return (
+        <>
+            <HomePageHeader />
+            <HomePageContent />
+        </>
+    );
+}
+
+export default HomePage
